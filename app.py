08facf7dc8d047c9c6705abedcf1b4034fced370
David P. <megver83@parabola.nu>
2025-06-26 19:52:08 -0400

    Muestra nuevo mensaje

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "¡Hola desde Flask en Azure App Service!"
+    return "<h1>¡Hola desde Flask en Azure App Service!</h1>\nProfesor, póngame un 7 😎"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
